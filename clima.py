eac4e20c68fed815751f9e5e5200e0399a5efebd
sofia <rendonsofi03@gmail.com>
2023-10-11 16:14:04 -0600

    visualizacion de columnas del archvio xls

--- clima.py
+++ clima.py
@@ -5,3 +5,6 @@ import matplotlib.pyplot as plt
 # Cargar el archivo XLS
 file_path = "C:/Users/rendo/Documents/proyectosING/Clima/temperatura.xls" 
 df = pd.read_excel(file_path)
+
+print(df.columns);
+
