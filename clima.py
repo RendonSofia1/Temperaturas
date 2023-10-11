757a7d7f983c33ff8df0b51240bfc37a02157028
sofia <rendonsofi03@gmail.com>
2023-10-11 16:07:11 -0600

    lecura archivos xls

--- clima.py
+++ clima.py
@@ -2,4 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-print ("gola");
+# Cargar el archivo XLS
+file_path = "C:/Users/rendo/Documents/proyectosING/Clima/temperatura.xls" 
+df = pd.read_excel(file_path)
